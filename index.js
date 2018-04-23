a92bd9a5e492af7f17895b00bcd8a68198e77ef4
Huy Dang <hqdang97@gmail.com>
2018-04-22 23:22:33 -0600

    deployment change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+// const port = process.env.PORT || 80;
 const port = 8080;
 const cors = require("cors");
 const bodyParser = require("body-parser");
